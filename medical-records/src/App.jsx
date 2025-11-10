e7f690dc67f047195616dae15f1ebda271304fe4
Melanie Ballard <132787498+melanieballard@users.noreply.github.com>
2025-11-10 17:58:07 -0500

    Mergeconflicts (#18)
    Co-authored-by: owenmoloney <owenmoloney7@gmail.com>
    Co-authored-by: Melanie Ballard <mballard7@storm.cis.fordham.edu>

--- medical-records/src/App.jsx
+++ medical-records/src/App.jsx
@@ -1,21 +1,19 @@
+
+// src/App.jsx
+// App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import AppNavbar from "./components/Navbar";
-import Home from "./components/Home";
-import Database from "./components/Database";
-import NewPatient from "./components/NewPatient";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import PatientPage from "./pages/PatientPage";
+import DoctorPage from "./pages/DoctorPage";
 
 export default function App() {
   return (
-    <Router>
-      <AppNavbar />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/data" element={<Database />} />
-          <Route path="/patients" element={<NewPatient />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/patient" element={<PatientPage />} />
+      <Route path="/doctor" element={<DoctorPage />} />
+    </Routes>
   );
 }
+
